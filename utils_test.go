d88742536f46044d87c0537de3ba6b853319e02f
peterChen <26huitailang@gmail.com>
2019-06-06 17:44:51 +0800

    Feat/rebase (#5)
    * readme: todo
    
    * 不能build for arm的问题
    
    * docker build for arm
    
    * echo替换未完成
    
    * echo 怎么用现在的方法解析模板？换一种写法？
    
    * 模板无法正常渲染，还要用echo吗？
    
    * echo 迁移完成，但是这个模版真的很不优雅
    
    * makefile: cover profile

--- utils_test.go
+++ utils_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func BenchmarkInitTheme(b *testing.B) {
-	InitConfiguration()
+	initConfiguration()
 	f, err := os.Create("cpu.prof")
 	if err != nil {
 		b.Fatal(err)
@@ -15,6 +15,6 @@ func BenchmarkInitTheme(b *testing.B) {
 	pprof.StartCPUProfile(f)
 	defer pprof.StopCPUProfile()
 	for i := 0; i < b.N; i++ {
-		InitTheme(&config)
+		InitTheme(config)
 	}
 }
