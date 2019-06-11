31dbb1c32d4232f9308208b363674243d9b9b3ab
26huitailang <26huitailang@gmail.com>
2019-06-11 15:04:13 +0800

    dolike, doread

--- views.go
+++ views.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/26huitailang/golang-web/models"
 	"github.com/labstack/echo"
 
-	// "github.com/labstack/gommon/log"
 	log "github.com/sirupsen/logrus"
 )
+
+func IndexHandle(c echo.Context) (err error) {
+	http.Redirect(c.Response(), c.Request(), "/themes", 301)
+	return nil
+}
 
 func ThemesHandle(c echo.Context) (err error) {
 	var themes []models.Theme
@@ -46,14 +51,36 @@ func ThemeHandle(c echo.Context) (err error) {
 	}
 
 	log.Debugf("theme api suites[%s]: %v", theme.Name, suites)
+
+	var countRead, countUnread int
+	DB.Model(&models.Suite{}).Where("theme_id = ? and is_read = true", themeID).Count(&countRead)
+	DB.Model(&models.Suite{}).Where("theme_id = ? and is_read = false", themeID).Count(&countUnread)
 	data := struct {
-		Theme  models.Theme
-		Suites []models.Suite
+		Theme       models.Theme
+		Suites      []models.Suite
+		CountRead   int
+		CountUnread int
 	}{
 		theme,
 		suites,
+		countRead,
+		countUnread,
 	}
 	return c.Render(http.StatusOK, "layout:theme", data)
+}
+
+type suitesQuery struct {
+	IsLike bool `query:"is_like"`
+}
+
+func SuitesHandle(c echo.Context) (err error) {
+	query := new(suitesQuery)
+	if err = c.Bind(query); err != nil {
+		return
+	}
+	var suites []models.Suite
+	DB.Where("is_like = ?", query.IsLike).Find(&suites)
+	return c.Render(200, "layout:suites", suites)
 }
 
 func SuiteHandle(c echo.Context) (err error) {
@@ -70,16 +97,46 @@ func SuiteHandle(c echo.Context) (err error) {
 	DB.Model(&suite).Related(&images).Order("name")
 	log.Debugf("images: %v", images)
 	data := struct {
-		Name   string
+		Suite  models.Suite
 		Images []models.Image
 	}{
-		suite.Name,
+		suite,
 		images,
 	}
 	return c.Render(200, "layout:suite", data)
 }
 
+func SuiteReadHandle(c echo.Context) (err error) {
+	var suiteID int
+	suiteID, err = strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.Redirect(http.StatusNotFound, "/")
+	}
+	var suite models.Suite
+	DB.First(&suite, suiteID)
+	suite.IsRead = !suite.IsRead
+	DB.Save(&suite)
+	url := fmt.Sprintf("/suites/%d#%s", suiteID, "action-read")
+	log.Debugln("redirect:", url)
+	return c.Redirect(302, url)
+}
+
 // todo: /suite/:id/like 翻转操作，时间限制，3s一次
+func SuiteLikeHandle(c echo.Context) (err error) {
+	var suiteID int
+	suiteID, err = strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.Redirect(http.StatusNotFound, "/")
+	}
+	var suite models.Suite
+	DB.First(&suite, suiteID)
+	suite.IsLike = !suite.IsLike
+	suite.IsRead = true
+	DB.Save(&suite)
+	url := fmt.Sprintf("/suites/%d#%s", suiteID, "action-like")
+	log.Debugln("redirect:", url)
+	return c.Redirect(302, url)
+}
 
 // InitDBHandle is view to init database depends on local files
 func InitDBHandle(c echo.Context) (err error) {
