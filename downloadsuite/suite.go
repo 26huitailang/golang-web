dc1a8efe5bcf5147a3588aff9180daf3366a118f
26huitailang <26huitailang@gmail.com>
2020-03-23 00:02:27 +0800

    :hankey: Writing bad code that needs to be improved.
    - 尝试重新设计suite模块

--- downloadsuite/suite.go
+++ downloadsuite/suite.go
@@ -5,6 +5,11 @@ Package downloadsuite to download one downloadsuite images.
 */
 package downloadsuite
 
+import (
+	"errors"
+	"strconv"
+)
+
 // ISuiteOperator interface 定义了Suite的操作集合
 type ISuiteOperator interface {
 	//GetPageURLs(chan string)
@@ -20,5 +25,44 @@ type Suite struct {
 func NewSuite(operator ISuiteOperator) *Suite {
 	return &Suite{
 		Operator: operator,
+	}
+}
+
+// todo: can this work
+// NewSuiteV2 to be a factory to produce suite manager depends on url host
+func NewSuiteV2(url string) (ISuite, error) {
+	switch url {
+	case "mtr":
+		return NewSuiteDemo(url), nil
+	default:
+		return nil, errors.New("now")
+	}
+}
+
+type ISuite interface {
+	Parse() // parse the image urls/title...
+	Download()
+}
+
+type SuiteDemo struct {
+	images []string
+	title  string
+	url    string
+}
+
+func NewSuiteDemo(url string) ISuite {
+	return &SuiteDemo{url: url}
+}
+func (sd *SuiteDemo) Parse() {
+	println("parse")
+	for i := 0; i < 10; i++ {
+		sd.images = append(sd.images, strconv.Itoa(i))
+	}
+}
+
+func (sd *SuiteDemo) Download() {
+	println("download")
+	for _, item := range sd.images {
+		println(item)
 	}
 }
