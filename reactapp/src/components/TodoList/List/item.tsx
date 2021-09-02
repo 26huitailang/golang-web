1877524de44cc89d483e691724663b7992af1d3a
26huitailang <26huitailang@gmail.com>
2021-09-02 22:27:25 +0800

    chore: remove umi framework, use simple react

---
+++ reactapp/src/components/TodoList/List/item.tsx
@@ -0,0 +1,16 @@
+import React, {FC, ReactElement} from "react";
+import {ITodo} from "../typings";
+
+interface IProps {
+    item: ITodo
+}
+
+const TdItem: FC<IProps> = ({item}): ReactElement => {
+    return (
+        <div className="td-item">
+            {item.id}-{item.content}-{item.completed}
+        </div>
+    )
+}
+
+export default TdItem;
