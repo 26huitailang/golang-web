d70f470cbf2b7a2afb3a6a947bd6ce39141e21fa
26huitailang <26huitailang@gmail.com>
2021-09-03 21:45:30 +0800

    chore: eslint

--- reactapp/src/components/Login/index.tsx
+++ reactapp/src/components/Login/index.tsx
@@ -1,25 +1,31 @@
-import React, { FC, ReactElement, useState } from "react";
+import React, { FC, ReactElement, useState } from 'react';
 
 const Login: FC = (): ReactElement => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const handleSubmit = (event: any) => {
-    event.preventDefault()
-    console.log(username, password)
-    //TODO 账户密码校验
+    event.preventDefault();
+    console.log(username, password);
+    // TODO 账户密码校验
     // todo 请求服务器，获取token，变更app的认证状态
-  }
+  };
   return (
     <div>
-      <form onSubmit={event => handleSubmit(event)}>
-        <input type="text" className="username" value={username}
-          onChange={event => setUsername(event.target.value)}></input>
-        <input type="password" className="password" value={password}
-          onChange={event => setPassword(event.target.value)}></input>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <input
+          type="text"
+          className="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)} />
+        <input
+          type="password"
+          className="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)} />
         <button type="submit">登录</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
